e7747caee8ffe027cc1ce57adf10434d6df220d3
Kentaro Abe <kentaro@beah.jp>
2020-12-23 05:35:56 +0900

    APIキー生成機能

---
+++ app/controllers/authorization_controller.rb
@@ -0,0 +1,28 @@
+require 'jwt'
+require 'digest/sha2'
+class AuthorizationController < ApplicationController
+  def generate_token
+    if !params[:device_type].present? then
+      render json:{"status" => "error", "description" => "device_type is required"}, status: 400
+      return
+    end
+    
+    d = Device.new()
+    d.id = SecureRandom.uuid
+    d.device_type = params[:device_type]
+    
+    token = SecureRandom.alphanumeric(32)
+    d.hashed_token = Digest::SHA256.hexdigest(token)
+    
+    d.save()
+    
+    encoded_jwt = JWT.encode({
+                               "id" => d.id,
+                               "token" => token,
+                               "device_type" => d.device_type,
+                               "iss" => d.created_at.to_i
+                             }, ENV["JWT_SECRET"], "HS256")
+    
+    render json: {"status" => "success", "token" => encoded_jwt}
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,9 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  
+  scope :api do
+    scope :auth do
+      post 'issue-key', to: 'authorization#generate_token'
+    end
+  end
 end
