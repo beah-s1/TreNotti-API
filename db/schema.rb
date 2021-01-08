abdec96452862721b8b7e44833d109717ed0d508
Kentaro Abe <kentaro@beah.jp>
2021-01-09 05:03:51 +0900

    運行情報の収集、配信、保存処理

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_12_25_055434) do
+ActiveRecord::Schema.define(version: 2021_01_08_073632) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -22,6 +22,14 @@ ActiveRecord::Schema.define(version: 2020_12_25_055434) do
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.string "railways", default: [], null: false, array: true
+  end
+
+  create_table "train_statuses", id: :string, force: :cascade do |t|
+    t.string "railway", default: "", null: false
+    t.string "operator", default: "", null: false
+    t.jsonb "status", default: {}, null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
   end
 
 end
