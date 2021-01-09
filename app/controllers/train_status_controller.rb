ed3e5503b4a83c54980d2c0d89d725aa6884b9df
Kentaro Abe <kentaro@beah.jp>
2021-01-09 21:57:19 +0900

    サーバーでキャッシュしている運行情報を返すAPI

--- app/controllers/train_status_controller.rb
+++ app/controllers/train_status_controller.rb
@@ -1,2 +1,17 @@
 class TrainStatusController < ApplicationController
+  before_action :verify_token
+  
+  def index
+    status = TrainStatus.all()
+    
+    payload = []
+    
+    status.each do |s|
+      # 路線情報がない場合は、路線情報を付加する
+      s.status["odpt:railway"] = s.railway if !s.status["odpt:railway"].present?
+      payload.append(s.status)
+    end
+    
+    render json: payload
+  end
 end
